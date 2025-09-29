c290c12f9f32e7c8675788027668cf7e9f416628
littleneogul <hhw9560@naver.com>
2025-09-29 14:13:39 +0900

    v0.2 types/main.go

--- week06/types/main.go
+++ week06/types/main.go
@@ -9,4 +9,6 @@ import (
 func main() {
 	fmt.Println(math.Ceil(2.31))
 	fmt.Println(strings.Title("go developer~"))
+	fmt.Println("Kim\nInha\t\"\\") // C linke
+	fmt.Println('A', '가')          // Rune (Unicode)
 }

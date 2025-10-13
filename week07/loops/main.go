0569de2845bfb9eb600daa5df8e11aa5a6efcde8
littleneogul <hhw9560@naver.com>
2025-10-13 15:22:54 +0900

    loops/main.go v0.1 strconv package

--- week07/loops/main.go
+++ week07/loops/main.go
@@ -5,15 +5,29 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
+	fmt.Print("Enter a grade: ")
 	r := bufio.NewReader(os.Stdin)
 	i, err := r.ReadString('\n')
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(i)``
+	i = strings.TrimSpace(i)                // trim 함수
+	score, err := strconv.ParseFloat(i, 64) // 정리된 문자열을 실수타입으로 변환
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var status string
+	if score >= 60 {
+		status = "Pass"
+	} else {
+		status = "Fail"
+	}
+	fmt.Println(score, "is", status)
 }

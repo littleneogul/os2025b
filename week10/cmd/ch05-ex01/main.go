0d2eacf3c7e907e29fbc71d4fb1e834f9294cd03
littleneogul <hhw9560@naver.com>
2025-11-03 15:30:36 +0900

    week10 array v0.2

--- week10/cmd/ch05-ex01/main.go
+++ week10/cmd/ch05-ex01/main.go
@@ -3,11 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	var arrayBool [3]bool
+	var arrayBool [3]bool = [3]bool{true, false, true}
 	var arrayInt [3]int
 
-	fmt.Println(arrayBool[1]) // zero value (false)
-	arrayInt[1]++
-	arrayInt[1] = arrayInt[1] + 1
+	fmt.Println(arrayBool[1])
+	arrayInt[1] = 2
 	fmt.Println(arrayInt[1])
 }

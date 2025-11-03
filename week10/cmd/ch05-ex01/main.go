db256b1a71817f36c5a98ed4d70526f7328e8d05
littleneogul <hhw9560@naver.com>
2025-11-03 15:25:02 +0900

    week10 array v0.1

---
+++ week10/cmd/ch05-ex01/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arrayBool [3]bool
+	var arrayInt [3]int
+
+	fmt.Println(arrayBool[1]) // zero value (false)
+	arrayInt[1]++
+	arrayInt[1] = arrayInt[1] + 1
+	fmt.Println(arrayInt[1])
+}

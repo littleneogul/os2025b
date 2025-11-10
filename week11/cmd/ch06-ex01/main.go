ebe58a39beb9692aaeb064d4effdf16f5ef9a468
littleneogul <hhw9560@naver.com>
2025-11-10 15:00:17 +0900

    week11 slice v0.5

--- week11/cmd/ch06-ex01/main.go
+++ week11/cmd/ch06-ex01/main.go
@@ -7,8 +7,9 @@ func main() {
 	subjectsSlice := subjects[:3] // slicing
 
 	subjects[0] = "Java"
-
 	// subjectsSlice[0] = "Java"
+
+	subjectsSlice = append(subjectsSlice, "Go")
 
 	for _, subject := range subjects {
 		fmt.Println(subject)

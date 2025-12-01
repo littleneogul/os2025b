ecb679fa6b2141d6623c3e663319466a8fd9c7ae
littleneogul <hhw9560@naver.com>
2025-12-01 14:26:15 +0900

    week14 defer v0.2

--- week14/cmd/ch12-ex01/main.go
+++ week14/cmd/ch12-ex01/main.go
@@ -1,20 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"os"
-)
+import "fmt"
 
 func main() {
-	file, err := os.Open("test.txt")
-	if err != nil {
-		fmt.Println("파일 오픈 실패:", err)
-		return
-	}
-
-	// defer로 파일 닫기를 미리 예약
-	defer file.Close()
-
-	// 파일 작업 수행
-	fmt.Println("파일 읽는 중...")
+	defer fmt.Println("1st defer")
+	defer fmt.Println("2nd defer")
+	defer fmt.Println("3rd defer")
+	fmt.Println("main logic")
 }
